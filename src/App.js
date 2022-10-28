655a1c40220e2baba7d4ede2b8c47ca45e7a41ba
waleedr7643 <59436106+waleedr7643@users.noreply.github.com>
2022-10-28 19:32:09 +0500

    new project push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React, { Component } from 'react'
+import Navbar { Component } from 'react'
 
 export default class App extends Component {
   c ='john';
   render() {
     return (
       <div>
-        Hello my first class ase Component {this.c}
+      <Navbar />
       </div>
     )
   }
